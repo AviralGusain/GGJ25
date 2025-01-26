95081cda2d742e2aa2b15e7dbb758ea7b49c3980
jaredacl <jaredcallupelopez@gmail.com>
2025-01-26 14:03:40 -0800

    - Various menu updates and animations - Added several art assets

---
+++ Assets/Jared/LevelSelect.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+    public Button CloseButton;
+    private MainMenu MM;
+
+    private void Start()
+    {
+        MM = FindAnyObjectByType<MainMenu>();
+        CloseButton.onClick.AddListener(Close);
+    }
+
+    public void Close()
+    {
+        MM.DestroySubmenus();
+    }
+
+    public void OpenLevel(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
