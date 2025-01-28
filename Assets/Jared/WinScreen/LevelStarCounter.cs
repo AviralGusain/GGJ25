4432ae82811deab1c71cbd3762b1dae4b9f3354f
Riley Durbin <111633437+RileyDurb@users.noreply.github.com>
2025-01-27 23:54:03 -0800

    made level star prefab update

--- Assets/Jared/WinScreen/LevelStarCounter.cs
+++ Assets/Jared/WinScreen/LevelStarCounter.cs
@@ -5,9 +5,16 @@ public class LevelStarCounter : MonoBehaviour
     public Transform StarPanel;
     public GameObject StarPrefab;
     public int StarCount = 0;
+    public string LevelName = "Level1";
 
     private void Start()
     {
+        PlayerScores scores = FindFirstObjectByType<PlayerScores>();
+        if (scores != null && LevelName != null)
+        {
+            StarCount = scores.GetLevelScore(LevelName).numBubbles;
+        }
+
         for (int i = 0; i < StarCount; i++)
         {
             Instantiate(StarPrefab, StarPanel);
